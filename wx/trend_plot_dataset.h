b974280eb5063e070f54cd7ba2e28827271e86ff
bradford.elliott <bradford.elliott@82304e6a-2c44-0410-8db0-b791f402dd04>
2008-01-22 03:22:55 +0000

    Added start of trend plot component

---
+++ wx/trend_plot_dataset.h
@@ -0,0 +1,19 @@
+#ifndef __TREND_PLOT_DATASET_H__
+#define __TREND_PLOT_DATASET_H__
+
+#include <string>
+#include <map>
+#include <wx/colour.h>
+
+class wxTrendPlotDataset
+{
+    public:
+        std::string              m_label;
+        std::map<double, double> m_points;
+        wxColour                 m_color;
+        
+        int m_delete_menu_id;
+};
+
+#endif // __TREND_PLOT_DATASET_H__
+
